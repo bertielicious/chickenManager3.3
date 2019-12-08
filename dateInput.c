8c7b0297046e55e29c12c4baeeba5bace395d07a
Phil Glazzard <phil@hifigear.co.uk>
2019-12-08 20:45:38 +0000

    Add files via upload

---
+++ dateInput.c
@@ -0,0 +1,36 @@
+#include "config.h"
+#include "byteToBin.h"
+void dateInput(void)
+{
+    uchar date[] = {"enter date"};
+    uchar month[6] = {"month"};
+    uchar day[] = {"day"};
+    uchar yr[] = {"year"};
+    uchar *ptr;
+    
+    ptr= &date[0];
+    byteToBin(0,0x80);  // 2nd line position 0
+    while( *ptr > 0)
+    {
+      byteToBin(1, *ptr);
+      ptr++;
+    }
+    
+    ptr = &month[0];
+    byteToBin(0,0x94);  // 3rd line position 0
+    while( *ptr > 0)
+      {
+        byteToBin(1, *ptr);
+        ptr++;
+      }
+    
+    ptr = &day[0];
+    byteToBin(0,0xd4);  // 4th line position 0
+    while( *ptr > 0)
+       {
+        byteToBin(1, *ptr);
+        ptr++;
+       }
+            
+    
+}
